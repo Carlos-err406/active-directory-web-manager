946149b94265453a67c18a7b5f01af3acef534a7
Carlos Daniel Vilaseca <carlosd.vilaseca@generalsoftwareinc.com>
2024-04-06 14:40:46 -0400

    feat: protected routes layout

--- src/routes/(protected)/+layout.svelte
+++ src/routes/(protected)/+layout.svelte
@@ -1,1 +1,16 @@
+<script lang="ts">
+	import Header from '$lib/components/layout/header.svelte';
+	import Navigation from '$lib/components/layout/navigation.svelte';
+</script>
+
+<div class="flex min-h-screen w-full flex-col bg-muted/40">
+	<Navigation />
+	<div class="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+		<Header />
+		<main class="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+            <slot />
+		</main>
+	</div>
+</div>
+
 <slot />
