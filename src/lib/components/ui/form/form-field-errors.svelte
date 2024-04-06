557533a8aae06421b1365c7fb774a7ea892aa208
Carlos Daniel Vilaseca <carlosd.vilaseca@generalsoftwareinc.com>
2024-04-06 11:39:26 -0400

    feat(ui): shadcn components

---
+++ src/lib/components/ui/form/form-field-errors.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import * as FormPrimitive from 'formsnap';
+	import { cn } from '$lib/utils.js';
+	import { slide } from 'svelte/transition';
+
+	type $$Props = FormPrimitive.FieldErrorsProps & {
+		errorClasses?: string | undefined | null;
+	};
+
+	let className: $$Props['class'] = undefined;
+	export { className as class };
+	export let errorClasses: $$Props['class'] = undefined;
+</script>
+
+<FormPrimitive.FieldErrors
+	class={cn('text-[0.8rem] font-medium text-destructive ', className)}
+	{...$$restProps}
+	let:errors
+	let:fieldErrorsAttrs
+	let:errorAttrs
+>
+	<slot {errors} {fieldErrorsAttrs} {errorAttrs}>
+		{#each errors as error}
+			<div transition:slide={{ axis: 'y', duration: 150 }} {...errorAttrs} class={cn(errorClasses)}>
+				{error}
+			</div>
+		{/each}
+	</slot>
+</FormPrimitive.FieldErrors>
---
+++ src/lib/components/ui/loader.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import clsx from 'clsx';
+
+	export let condition = false;
+	export let vertical = false;
+	export let size: 'sm' | 'md' | 'lg' = 'sm';
+	export let amount = 3;
+</script>
+
+{#if !condition && condition !== undefined}
+	<slot />
+{:else}
+	<div class={clsx('flex', 'gap-1', vertical ? 'flex-col' : 'flex-row')}>
+		{#each Array(amount) as _, index}
+			<div
+				class={clsx(
+					'relative flex aspect-square items-center justify-center rounded-full border-[1px] border-slate-500',
+					size === 'sm' ? 'h-3' : size === 'md' ? 'h-5' : 'h-8'
+				)}
+			>
+				<div
+					class={clsx(
+						'absolute aspect-square animate-ping rounded-full border-[1px] border-slate-500',
+						'delay-' + index * 100,
+						size === 'sm' ? 'h-3' : size === 'md' ? 'h-5' : 'h-8'
+					)}
+				></div>
+			</div>
+		{/each}
+	</div>
+{/if}
