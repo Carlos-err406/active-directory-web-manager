0f0b3d13f4d6b53bacb6b6b99cdb9fdd8b26b64c
Carlos Daniel Vilaseca <carlosd.vilaseca@generalsoftwareinc.com>
2024-04-06 14:36:34 -0400

    feat: init stores

---
+++ src/lib/stores/index.ts
@@ -0,0 +1,19 @@
+import { writable } from 'svelte/store';
+
+const getBreadcrumbsStore = () => {
+	interface Breadcrumb {
+		name: string;
+		link?: string;
+	}
+	const { subscribe, set } = writable<Breadcrumb[]>([{ name: 'Console' }]);
+	const _set = (value: Breadcrumb[]) => {
+		set([{ name: 'Console' }, ...value]);
+	};
+
+	return {
+		subscribe,
+		set: _set
+	};
+};
+
+export const breadcrumbs = getBreadcrumbsStore();
