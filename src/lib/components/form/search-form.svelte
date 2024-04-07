536eaf0298fe976660e8e0bb8b3891b953c7548a
Carlos Daniel Vilaseca <carlosd.vilaseca@generalsoftwareinc.com>
2024-04-07 00:56:58 -0400

    feat: search form implementation enhanced

--- src/lib/components/form/search-form.svelte
+++ src/lib/components/form/search-form.svelte
@@ -1,26 +1,83 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
+	import { afterNavigate, goto } from '$app/navigation';
 	import { page } from '$app/stores';
 	import { searchSchema } from '$lib/schemas/search-schema';
 	import { Search } from 'lucide-svelte';
+	import { onDestroy } from 'svelte';
 	import { superForm } from 'sveltekit-superforms';
 	import { zodClient } from 'sveltekit-superforms/adapters';
+	import Timer from '../ui/timer/timer.svelte';
 	import Input from './input.svelte';
-	const methods = superForm($page.data.form || { q: '' }, {
-		validators: zodClient(searchSchema)
+	let timer: number;
+	let showTimer = false;
+	let formElement: HTMLFormElement;
+	let q = $page.url.searchParams.get('q') || '';
+	afterNavigate(() => {
+		q = $page.url.searchParams.get('q') || '';
 	});
-	const { form: formData } = methods;
+	$: methods = superForm(
+		{ q },
+		{
+			validators: zodClient(searchSchema),
+			clearOnSubmit: 'errors',
+
+			onSubmit: async (event) => {
+				const q = event.formData.get('q') as string;
+				const search = $page.url.searchParams;
+				if (q) search.set('q', q);
+				else search.delete('q');
+				await goto($page.url.pathname + '?' + search.toString());
+			}
+		}
+	);
+	$: ({ form: formData, submit, enhance } = methods);
+
+	const handleInput = () => {
+		clearTimeout(timer);
+		if (showTimer) {
+			showTimer = false;
+			setTimeout(() => (showTimer = true), 10);
+		} else showTimer = true;
+		timer = setTimeout(() => {
+			showTimer = false;
+			submit(formElement);
+		}, 1000);
+	};
+	const handleEnter = (event: KeyboardEvent) => {
+		if (event.key === 'Enter') {
+			clearTimeout(timer);
+			showTimer = false;
+			submit(formElement);
+		}
+	};
+	onDestroy(() => {
+		clearTimeout(timer);
+		showTimer = false;
+	});
 </script>
 
-<form class="min-w-fit max-w-64" action="?/search" method="post" use:enhance>
+<form
+	bind:this={formElement}
+	class="w-full min-w-fit max-w-96"
+	action="?/search"
+	method="post"
+	use:enhance
+>
 	<Input
 		name="q"
 		{methods}
-		value={$formData.q}
-		inputProps={{ type: 'search', placeholder: 'Search' }}
+		bind:value={$formData.q}
+		on:input={handleInput}
+		on:keydown={handleEnter}
+		inputProps={{ type: 'text', placeholder: 'Search' }}
 	>
 		<svelte:fragment slot="addornment-left">
 			<Search />
+		</svelte:fragment>
+		<svelte:fragment slot="addornment-right">
+			{#if showTimer}
+				<Timer timeout={1000} />
+			{/if}
 		</svelte:fragment>
 	</Input>
 </form>
---
+++ src/lib/components/ui/timer/timer.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { flyAndScale } from '$lib/utils';
+	import { createEventDispatcher, onMount } from 'svelte';
+	import { scale } from 'svelte/transition';
+
+	export let timeout: number;
+	let animationFrameId: number;
+	let startTime: number;
+	let rotation = 0;
+	const dispatch = createEventDispatcher();
+	onMount(() => {
+		animationFrameId = requestAnimationFrame(animate);
+		return () => cancelAnimationFrame(animationFrameId);
+	});
+	const animate = (timestamp: number) => {
+		if (startTime === undefined) {
+			startTime = timestamp;
+		}
+		const timeElapsed = timestamp - startTime;
+
+		if (timeElapsed < timeout) {
+			const rotationValue = (360 * timeElapsed) / timeout;
+			rotation = rotationValue;
+			animationFrameId = requestAnimationFrame(animate);
+		} else {
+			rotation = 360;
+			dispatch('completed');
+			// The animation has completed; so we don't request another frame here
+			return; // Exit the function to prevent requesting another animation frame
+		}
+	};
+</script>
+
+<div class="h-5 w-5">
+	<svg viewBox="0 0 85 85" xmlns="http://www.w3.org/2000/svg">
+		<circle cx="50" cy="50" r="30" fill="none" stroke="black" />
+		<line x1="25" y1="15" x2="75" y2="15" stroke="black" />
+		<line x1="50" y1="50" x2="50" y2="25" stroke="black" transform={`rotate(${rotation} 50 50)`} />
+	</svg>
+</div>
