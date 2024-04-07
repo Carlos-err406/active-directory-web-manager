6afa9b1bb5de21e1bd0108e9c3f321649f177eb5
Carlos Daniel Vilaseca <carlosd.vilaseca@generalsoftwareinc.com>
2024-04-07 15:27:53 -0400

    feat: search action in main routes

---
+++ src/routes/(protected)/logs/+page.server.ts
@@ -0,0 +1,17 @@
+import { superValidate } from 'sveltekit-superforms';
+import type { Actions, PageServerLoad } from './$types';
+import { searchSchema } from '$lib/schemas/search-schema';
+import { zod } from 'sveltekit-superforms/adapters';
+
+export const load: PageServerLoad = async ({ url }) => {
+	const q = url.searchParams.get('q');
+	console.log({ logs: q });
+	return {};
+};
+
+export const actions: Actions = {
+	search: async (event) => {
+		const form = await superValidate(event, zod(searchSchema));
+		return { form };
+	}
+};
