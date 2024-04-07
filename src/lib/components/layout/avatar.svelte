0c40dd22c462dd545605a251c912a3815faf07e3
Carlos Daniel Vilaseca <carlosd.vilaseca@generalsoftwareinc.com>
2024-04-07 00:53:03 -0400

    feat: signOut

--- src/lib/components/layout/avatar.svelte
+++ src/lib/components/layout/avatar.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
-	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
 	import { Button } from '$lib/components/ui/button';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
 </script>
 
 <DropdownMenu.Root>
 	<DropdownMenu.Trigger asChild let:builder>
 		<Button builders={[builder]} variant="outline" size="icon" class="overflow-hidden rounded-full">
-			<img src="/user-placeholder.jpg" width={36} height={36} alt="Avatar" class="overflow-hidden" />
+			<img
+				src="/user-placeholder.jpg"
+				width={36}
+				height={36}
+				alt="Avatar"
+				class="overflow-hidden"
+			/>
 		</Button>
 	</DropdownMenu.Trigger>
 	<DropdownMenu.Content align="end">
@@ -15,6 +21,10 @@
 		<DropdownMenu.Item>Settings</DropdownMenu.Item>
 		<DropdownMenu.Item>Support</DropdownMenu.Item>
 		<DropdownMenu.Separator />
-		<DropdownMenu.Item>Logout</DropdownMenu.Item>
+		<DropdownMenu.Item class="justify-center p-0">
+			<form method="post" action="/?/signOut" class="w-full">
+				<Button type="submit" class="w-full">Logout</Button>
+			</form>
+		</DropdownMenu.Item>
 	</DropdownMenu.Content>
 </DropdownMenu.Root>
