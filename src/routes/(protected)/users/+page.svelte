0fdbe0d475d7ec1de871fdc8e95fc9f8df2d1e80
Carlos Daniel Vilaseca <carlosd.vilaseca@avangenio.com>
2024-05-11 13:22:32 -0400

    rf: using data table abstraction in users view

--- src/routes/(protected)/users/+page.svelte
+++ src/routes/(protected)/users/+page.svelte
@@ -1,20 +1,27 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import ColumnsDropdown from '$lib/components/table/columns-dropdown.svelte';
+	import DataTable from '$lib/components/table/data-table.svelte';
 	import UserTableActions from '$lib/components/table/user-table-actions.svelte';
-	import { Button } from '$lib/components/ui/button';
 	import * as Table from '$lib/components/ui/table';
-	import ChevronUp from '$lucide/chevron-up.svelte';
-	import ChevronDown from '$lucide/chevron-down.svelte';
-	import { Render, Subscribe, createRender, createTable } from 'svelte-headless-table';
-	import { addSortBy } from 'svelte-headless-table/plugins';
-	import { get, readable } from 'svelte/store';
+	import { createRender, createTable } from 'svelte-headless-table';
+	import { addHiddenColumns, addSortBy } from 'svelte-headless-table/plugins';
+	import { derived, readable } from 'svelte/store';
 	import type { PageData } from './$types';
 	export let data: PageData;
 
 	$: ({ pagination } = data);
+	const hiddenColumnIds = derived(
+		page,
+		($page) => $page.url.searchParams.get('hide')?.split(',') || []
+	);
+
 	$: table = createTable(readable(pagination.data), {
 		sort: addSortBy({
 			disableMultiSort: true
+		}),
+		hide: addHiddenColumns({
+			initialHiddenColumnIds: $hiddenColumnIds
 		})
 	});
 
@@ -25,7 +32,7 @@
 		}),
 		table.column({
 			accessor: 'distinguishedName',
-			header: 'Distinguished Name'
+			header: 'DistinguishedName'
 		}),
 		table.column({
 			accessor: 'mail',
@@ -34,7 +41,7 @@
 		}),
 		table.column({
 			accessor: ({ dn }) => dn,
-			header: 'Actions',
+			header: '',
 			plugins: {
 				sort: { disable: true }
 			},
@@ -44,72 +51,16 @@
 		})
 	]);
 
-	$: ({ headerRows, pageRows, tableAttrs, tableBodyAttrs } = table.createViewModel(columns));
-	$: ({ searchParams } = $page.url);
-
-	const setSortAndOrder = (id: string) => {
-		const currentParams = new URLSearchParams(searchParams);
-		currentParams.set('order', searchParams.get('order') === 'asc' ? 'desc' : 'asc');
-		currentParams.set('sort', id);
-		return currentParams;
-	};
+	$: ({ flatColumns, ...rest } = table.createViewModel(columns));
+	const hidableCols = ['sAMAccountName', 'distinguishedName', 'mail'];
 </script>
 
-<div class="w-full rounded-md border">
-	<Table.Root {...$tableAttrs}>
-		<Table.Header>
-			{#each $headerRows as headerRow}
-				<Subscribe rowAttrs={headerRow.attrs()}>
-					<Table.Row>
-						{#each headerRow.cells as cell (cell.id)}
-							<Subscribe attrs={cell.attrs()} let:attrs let:props props={cell.props()}>
-								<Table.Head {...attrs}>
-									{#if props.sort.disabled}
-										<Render of={cell.render()} />
-									{:else}
-										{@const currentParams = setSortAndOrder(cell.id)}
-										<Button
-											variant="link"
-											href="/users?{currentParams.toString()}"
-											class="text-inherit hover:no-underline"
-											on:click={props.sort.toggle}
-										>
-											<Render of={cell.render()} />
-											{#if searchParams.get('sort') === cell.id && searchParams.get('order') === 'desc'}
-												<ChevronUp class="ml-2 size-4" />
-											{:else if searchParams.get('sort') === cell.id}
-												<ChevronDown class="ml-2 size-4" />
-											{:else}
-												<span class="ml-2 size-4" />
-											{/if}
-										</Button>
-									{/if}
-								</Table.Head>
-							</Subscribe>
-						{/each}
-					</Table.Row>
-				</Subscribe>
-			{/each}
-		</Table.Header>
-		<Table.Body {...$tableBodyAttrs}>
-			{#each $pageRows as row (row.id)}
-				<Subscribe rowAttrs={row.attrs()} let:rowAttrs>
-					<Table.Row {...rowAttrs}>
-						{#each row.cells as cell (cell.id)}
-							<Subscribe attrs={cell.attrs()} let:attrs>
-								<Table.Cell {...attrs}>
-									{#if cell.state?.data && get(cell.state.data) && cell.dataRowColId()}
-										<Render of={cell.render()} />
-									{:else}
-										-
-									{/if}
-								</Table.Cell>
-							</Subscribe>
-						{/each}
-					</Table.Row>
-				</Subscribe>
-			{/each}
-		</Table.Body>
-	</Table.Root>
+<div class="w-full">
+	<div class="my-2 flex w-full justify-end">
+		<ColumnsDropdown {hidableCols} {flatColumns} />
+	</div>
+	<div class="w-full rounded-md border">
+		<DataTable {...rest} />
+	</div>
 	<Table.Pagination />
 </div>
