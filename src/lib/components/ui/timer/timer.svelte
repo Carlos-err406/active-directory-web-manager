db42ab2686d590b3f80f5517fe812a55ade911d7
Carlos Daniel Vilaseca <carlosd.vilaseca@avangenio.com>
2024-05-11 15:36:59 -0400

    fix(styles): timer stroke color and width

--- src/lib/components/ui/timer/timer.svelte
+++ src/lib/components/ui/timer/timer.svelte
@@ -31,8 +31,8 @@
 
 <div class="h-5 w-5">
 	<svg viewBox="0 0 85 85" xmlns="http://www.w3.org/2000/svg">
-		<circle cx="50" cy="50" r="30" fill="none" stroke="black" />
-		<line x1="25" y1="15" x2="75" y2="15" stroke="black" />
-		<line x1="50" y1="50" x2="50" y2="25" stroke="black" transform={`rotate(${rotation} 50 50)`} />
+		<circle cx="50" cy="50" r="30" fill="none" stroke="currentColor"  stroke-width={6} />
+		<line x1="25" y1="15" x2="75" y2="15" stroke="currentColor"  stroke-width={6} />
+		<line x1="50" y1="50" x2="50" y2="25" stroke="currentColor"  stroke-width={6} transform={`rotate(${rotation} 50 50)`} />
 	</svg>
 </div>
