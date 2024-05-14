41e6718fb93dfaf94961caf032151349624f2e49
Carlos Daniel Vilaseca <carlosd.vilaseca@avangenio.com>
2024-05-13 23:32:46 -0400

    feat: user form actions for create, delete and delete many

--- src/routes/(protected)/users/+page.server.ts
+++ src/routes/(protected)/users/+page.server.ts
@@ -1,13 +1,28 @@
 import { PUBLIC_BASE_DN } from '$env/static/public';
 import { search } from '$lib/actions';
+import { encodePassword, getEntryByDn, getEntryBySAMAccountName } from '$lib/ldap';
 import { extractPagination, type PaginationWithUrls } from '$lib/pagination';
+import { createUserSchema } from '$lib/schemas/user/create-user-schema';
+import { deleteManyUsersSchema, deleteUserSchema } from '$lib/schemas/user/delete-user.schema';
 import type { User } from '$lib/types/user';
 import { error, redirect } from '@sveltejs/kit';
-import { AndFilter, EqualityFilter, NotFilter, SubstringFilter, type Filter } from 'ldapts';
+import {
+	AndFilter,
+	Attribute,
+	Change,
+	EqualityFilter,
+	NotFilter,
+	OrFilter,
+	SubstringFilter,
+	type Filter
+} from 'ldapts';
+import { fail, setError, superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
 import { v4 } from 'uuid';
 import type { Actions, PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ url, locals }) => {
+export const load: PageServerLoad = async ({ url, locals, depends }) => {
+	depends('protected:users');
 	const auth = await locals.auth();
 	if (!auth) throw redirect(302, '/');
 	const { ldap } = auth;
@@ -37,8 +52,8 @@ export const load: PageServerLoad = async ({ url, locals }) => {
 		await ldap.unbind();
 
 		searchEntries.sort((a, b) => {
-			if (a[sortAttribute] < b[sortAttribute]) return order === 'asc' ? -1 : 1;
-			if (a[sortAttribute] > b[sortAttribute]) return order === 'asc' ? 1 : -1;
+			if ((a[sortAttribute] || '-') < (b[sortAttribute] || '-')) return order === 'asc' ? -1 : 1;
+			if ((a[sortAttribute] || '-') > (b[sortAttribute] || '-')) return order === 'asc' ? 1 : -1;
 			return 0;
 		});
 		const pagination = extractPagination<User>(searchEntries as User[], page, pageSize);
@@ -55,7 +70,11 @@ export const load: PageServerLoad = async ({ url, locals }) => {
 				page >= pagination.totalPages ? null : `${pathname}?${nextPageSearchParams.toString()}`,
 			previousPage: page <= 1 ? null : `${pathname}?${previousPageSearchParams.toString()}`
 		};
-		return { pagination: paginationWithURLs };
+
+		return {
+			pagination: paginationWithURLs,
+			createUserForm: await superValidate(zod(createUserSchema))
+		};
 	} catch (e) {
 		const errorId = v4();
 		console.log({ errorId, e });
@@ -67,5 +86,116 @@ export const load: PageServerLoad = async ({ url, locals }) => {
 };
 
 export const actions: Actions = {
-	search
+	search,
+	create: async (event) => {
+		const { locals } = event;
+		const auth = await locals.auth();
+		if (!auth) throw redirect(302, '/');
+
+		const form = await superValidate(event, zod(createUserSchema));
+		if (!form.valid) return fail(400, { form });
+		const { ldap } = auth;
+		const { sAMAccountName, base, givenName, sn, mail, password, description } = form.data;
+
+		const [entry] = await getEntryBySAMAccountName(ldap, sAMAccountName);
+
+		if (entry) return setError(form, 'sAMAccountName', 'User already exists');
+
+		const attributes: Record<string, string | string[]> = {
+			objectClass: ['top', 'person', 'organizationalPerson', 'user'],
+			sAMAccountName,
+			userAccountControl: ['512'],
+			givenName,
+			mail
+		};
+		if (sn) attributes['sn'] = sn;
+		if (description) attributes['description'] = description;
+
+		const dn = `CN=${sAMAccountName},${base}`;
+
+		await ldap.add(dn, attributes);
+
+		const encodedPassword = encodePassword(password);
+		const passwordChange: Change = new Change({
+			operation: 'replace',
+			modification: new Attribute({ type: 'unicodePwd', values: [encodedPassword] })
+		});
+
+		try {
+			await ldap.modify(dn, passwordChange);
+		} catch (e) {
+			console.log(e);
+			console.log('deleting created user');
+			await ldap.del(dn);
+			throw error(500, "Something unexpected happened while creating setting the user's password");
+		}
+		const [createdEntry] = await getEntryByDn(ldap, dn);
+		console.log({ createdEntry });
+		await ldap.unbind();
+		return {
+			form,
+			success: true
+		};
+	},
+	delete: async (event) => {
+		const { locals } = event;
+		const auth = await locals.auth();
+		if (!auth) throw redirect(302, '/');
+		const form = await superValidate(event, zod(deleteUserSchema));
+		if (!form.valid) return fail(400, { form });
+		const { ldap } = auth;
+		const { dn } = form.data;
+
+		const filter = new EqualityFilter({ attribute: 'distinguishedName', value: dn });
+		const { searchEntries } = await ldap.search(PUBLIC_BASE_DN, {
+			filter: filter.toString()
+		});
+		const [entry] = searchEntries;
+		if (!entry) {
+			throw error(404, 'User not found');
+		} else if (entry['isCriticalSystemObject'] === 'TRUE') {
+			throw error(403, 'This entry can not be deleted!');
+		}
+		await ldap.del(dn);
+		await ldap.unbind();
+		return {
+			success: true
+		};
+	},
+	deleteMany: async (event) => {
+		const { locals } = event;
+		const auth = await locals.auth();
+		if (!auth) throw redirect(302, '/');
+		const form = await superValidate(event, zod(deleteManyUsersSchema));
+		if (!form.valid) return fail(400, { form });
+		const { ldap } = auth;
+		const { dns } = form.data;
+		const filter = new OrFilter({
+			filters: dns.map((dn) => new EqualityFilter({ attribute: 'distinguishedName', value: dn }))
+		});
+
+		const { searchEntries } = await ldap.search(PUBLIC_BASE_DN, {
+			filter: filter.toString()
+		});
+		const promises = searchEntries.map(async (entry) => {
+			if (entry['isCriticalSystemObject'] === 'TRUE') {
+				throw error(403, `Entry ${entry.sAMAccountName} can not be deleted!`);
+			}
+			return ldap.del(entry.dn).catch((e) => {
+				console.log(e);
+				throw error(
+					500,
+					`Something unexpected happened while deleting the user ${entry.sAMAccountName}`
+				);
+			});
+		});
+
+		await Promise.all(promises);
+
+		await ldap.unbind();
+		return {
+			form,
+			success: true
+		};
+	}
 };
