d206fb1bc4603a9e0b65a62ab29f39a1294a468a
Carlos Daniel Vilaseca <carlosd.vilaseca@avangenio.com>
2024-05-19 00:06:23 -0400

    chore: parsing user attribute before sending to client

--- src/routes/(protected)/users/[dn=dn]/+page.server.ts
+++ src/routes/(protected)/users/[dn=dn]/+page.server.ts
@@ -1,21 +1,21 @@
-import { PUBLIC_BASE_DN } from '$env/static/public';
+import { getEntryByDn } from '$lib/ldap';
 import type { User } from '$lib/types/user';
 import { error, redirect } from '@sveltejs/kit';
-import { EqualityFilter } from 'ldapts';
 import type { PageServerLoad } from './$types';
-
 export const load: PageServerLoad = async ({ params, locals }) => {
 	const auth = await locals.auth();
 	if (!auth) throw redirect(302, '/');
-
-	const { ldap } = auth;
+	const { ldap, session } = auth;
+	if (!session.isAdmin) throw error(403, 'You dont have access to this resource');
 	const { dn } = params;
-	const filter = new EqualityFilter({ attribute: 'distinguishedName', value: dn });
-	const { searchEntries } = await ldap.search(PUBLIC_BASE_DN, {
-		filter: filter.toString()
-	});
+	const user: User = await getEntryByDn<User>(ldap, dn);
 	await ldap.unbind();
-	if (searchEntries.length === 0) throw error(404, 'User not found');
+	if (!user) throw error(404, 'User not found');
 
-	return { user: searchEntries[0] as User };
+	if (user.jpegPhoto) {
+		const base64String = Buffer.from(user.jpegPhoto, 'base64').toString('base64');
+		user.jpegPhoto = `data:image/jpeg;base64,${base64String}`;
+	}
+
+	return { user, showSearch: false };
 };
