794df71c6a7b7b916df5ce8b5f65d14acad7ccb3
Carlos Daniel Vilaseca <carlosd.vilaseca@avangenio.com>
2024-05-24 18:36:14 -0400

    chore: rename hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -11,7 +11,7 @@ import type { Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 import { getLoggerHook } from 'sveltekit-logger-hook';
 
-const authenticationSetter: Handle = async ({ event, resolve }) => {
+const authenticationSetterHandler: Handle = async ({ event, resolve }) => {
 	const auth = async ({ cookies }: typeof event) => {
 		const access = getAccessToken(cookies);
 		if (!access) return null;
@@ -37,7 +37,7 @@ const authenticationSetter: Handle = async ({ event, resolve }) => {
 	return resolve(event);
 };
 
-const tryLdapUnbind: Handle = async ({ event, resolve }) => {
+const ldapUnbindHandler: Handle = async ({ event, resolve }) => {
 	const response = await resolve(event);
 	const { locals } = event;
 	const auth = await locals.auth();
@@ -51,26 +51,27 @@ const tryLdapUnbind: Handle = async ({ event, resolve }) => {
 	return response;
 };
 
+const logHandler = getLoggerHook({
+	template: '[{date}] {url}{urlSearchParams} {method} {status}',
+	decodeSearchParams: true,
+	colorOptions: {
+		date: ({ status }) => (status >= 400 ? 'redBold' : 'yellow'),
+		method: ({ status }) => (status >= 400 ? 'redBold' : 'green'),
+		status: ({ status }) => (status >= 400 ? 'redBold' : 'green'),
+		url: ({ status }) => (status >= 400 ? 'redBold' : 'cyanBold'),
+		urlSearchParams: ({ status }) => (status >= 400 ? 'redBold' : 'cyan')
+	}
+});
+
 const getSequence = () => {
 	const seq = [];
 	if (LOGGER && LOGGER === '1') {
 		console.log('LOGGING is enabled, set LOGGER .env variable to 0 to disable');
-		const logger = getLoggerHook({
-			template: '[{date}] {url}{urlSearchParams} {method} {status}',
-			decodeSearchParams: true,
-			colorOptions: {
-				date: ({ status }) => (status >= 400 ? 'redBold' : 'yellow'),
-				method: ({ status }) => (status >= 400 ? 'redBold' : 'green'),
-				status: ({ status }) => (status >= 400 ? 'redBold' : 'green'),
-				url: ({ status }) => (status >= 400 ? 'redBold' : 'cyanBold'),
-				urlSearchParams: ({ status }) => (status >= 400 ? 'redBold' : 'cyan')
-			}
-		});
-		seq.push(logger);
+		seq.push(logHandler);
 	} else {
 		console.log('LOGGING is disabled, set LOGGER .env variable to 1 to enable');
 	}
-	seq.push(authenticationSetter, tryLdapUnbind);
+	seq.push(authenticationSetterHandler, ldapUnbindHandler);
 	return seq;
 };
 
