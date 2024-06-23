4951b1c6297f35a74003dd1124893daf4060b474
Carlos Daniel Vilaseca <carlosd.vilaseca@avangenio.com>
2024-06-23 02:59:33 -0400

    ft: add loggs path to vite server fs config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import config from './src/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	server: {
+		fs: {
+			allow: [config.system.logging.basePath]
+		}
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
