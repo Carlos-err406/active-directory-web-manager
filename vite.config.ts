3d86061014e6e1d878054be12534d97e41149b9d
Carlos Daniel Vilaseca <carlosd.vilaseca@avangenio.com>
2024-06-24 23:28:40 -0400

    ft: hmr for config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,26 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import type { PluginOption } from 'vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), hotReloadOnConfigFileChange()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
 });
+function hotReloadOnConfigFileChange(): PluginOption {
+	const configFileRegex = new RegExp(/^.*(app\.config(.*).json)$/);
+	return {
+		name: 'custom-hmr',
+		enforce: 'post',
+		handleHotUpdate({ file, server }) {
+			if (configFileRegex.test(file)) {
+				console.log('reloading because config file changed...');
+				server.ws.send({
+					type: 'full-reload',
+					path: '*'
+				});
+			}
+		}
+	};
+}
