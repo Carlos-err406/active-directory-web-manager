2c102dcb7eec7e0ecdb16d201c2a9cc857c08f66
Carlos Daniel Vilaseca <carlosd.vilaseca@avangenio.com>
2024-12-19 12:13:45 -0500

    ft: add constraints to input and new textarea component

--- src/lib/components/form/text-area.svelte
+++ src/lib/components/form/text-area.svelte
@@ -1,41 +1,38 @@
 <script lang="ts" context="module">
-	import type { HTMLInputAttributes } from 'svelte/elements';
+	import type { HTMLTextareaAttributes } from 'svelte/elements';
 	import type { Methods } from './form.svelte';
-	export type InputProps = Partial<Omit<HTMLInputAttributes, 'value' | 'name'>>;
+	export type TextAreaProps = Partial<Omit<HTMLTextareaAttributes, 'value' | 'name'>>;
 </script>
 
 <script lang="ts">
 	import * as Form from '$lib/components/ui/form';
 	import { cn } from '$lib/utils';
-	import { Input } from '../ui/input';
+	import { Textarea } from '../ui/textarea';
 	export let methods: Methods;
-	const { form, constraints } = methods;
-
+	const { form } = methods;
 	export let name: keyof typeof $form;
-	export let inputProps: InputProps = {};
+	export let textAreaProps: TextAreaProps = {};
 </script>
 
 <Form.Field form={methods} {name}>
 	<Form.Control let:attrs>
-		<Form.Label class={cn(inputProps.required && 'required')}><slot name="label" /></Form.Label>
+		<Form.Label class={cn(textAreaProps.required && 'required')}><slot name="label" /></Form.Label>
 		<div class="relative">
 			<div class="adornment absolute left-2 top-2.5">
 				<slot name="adornment-left" />
 			</div>
-			<Input
+			<Textarea
 				class={cn(
 					!!$$slots['adornment-left'] && 'pl-10',
 					!!$$slots['adornment-right'] && 'pr-10',
 					'w-full'
 				)}
 				on:input
-				on:change
 				on:keydown
 				on:keypress
 				on:keyup
 				bind:value={$form[name]}
-				{...$constraints[name]}
-				{...inputProps}
+				{...textAreaProps}
 				{...attrs}
 			/>
 			<div class="adornment absolute right-2 top-2.5">
